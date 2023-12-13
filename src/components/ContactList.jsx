2e187dbaf9b99c83b5026c5ee973e07271e54637
Mary Sigmon <marysigmon@marys-mbp-2.mynetworksettings.com>
2023-12-13 12:47:58 -0500

    added fetch

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"; 
-
+import {useEffect} from 'react'
 import ContactRow from "./ContactRow";
 
 const dummyContacts = [
@@ -12,6 +12,20 @@ export default function ContactList() {
 
     const [contacts, setContacts] = useState(dummyContacts)
 
+    useEffect(()=>{
+        async function fetchContacts() {
+            try {
+                const response = await fetch(
+                    "https://jsonplace-univclone.herokuapp.com/users"
+                  );
+                  const result = await response.json();
+                  setContacts(result);
+            } catch (error) {
+              console.error(error);
+            }
+          }
+          fetchContacts()
+    },[])
 
     console.log("Contacts: ", contacts)
 
